934eabc72876b6e4f944dc492fa8fc480e809e53
LuukTo <luuk.torn@wur.nl>
2025-04-10 09:50:55 +0200

    Update explore_weather_data.R

--- explore_weather_data.R
+++ explore_weather_data.R
@@ -2,14 +2,23 @@
 
 # Load the required packages
 library(tidyverse)
+library(timetk)
 
 # Load the data in a tibble
 weather <- read_delim("data/raw/Deel_airport_weather_data/etmgeg_275_without_metadata.txt", 
                       delim = ",") 
 
-# Convert the YYYYMMDD column to a R date object
+# Convert the YYYYMMDD column to a R date object and rename to date
 
 weather <- weather %>% 
-  mutate(YYYYMMDD = as.Date(as.character(YYYYMMDD), format = "%Y%m%d"))
+  mutate(date = as.Date(as.character(YYYYMMDD), format = "%Y%m%d")) %>% 
+  select(-YYYYMMDD) %>% 
+  select(date, everything())
 
+
+# Do some exploration
+
+weather %>% 
+  group_by(UG) %>% 
+  summarize_by_time()
 
